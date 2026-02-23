cf7e2f987eeac3fc2d107c4fc3d58a53f09b5069
Victor <victorpavalache2004@gmail.com>
2026-02-23 14:51:27 +0200

    modificari

---
+++ lab22/train.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dropout 
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import concatenate
+
+from test_utils import summary, comparator
+
+import os
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import imageio
+
+import matplotlib.pyplot as plt
+from utils import process_path, preprocess
+
+path = ''
+image_path = os.path.join(path, './data/CameraRGB/')
+mask_path = os.path.join(path, './data/CameraMask/')
+image_list = os.listdir(image_path)
+mask_list = os.listdir(mask_path)
+image_list = [image_path+i for i in image_list]
+mask_list = [mask_path+i for i in mask_list]
+
+image_list_ds = tf.data.Dataset.list_files(image_list, shuffle=False)
+mask_list_ds = tf.data.Dataset.list_files(mask_list, shuffle=False)
+
+for path in zip(image_list_ds.take(3), mask_list_ds.take(3)):
+    print(path)
+
+image_filenames = tf.constant(image_list)
+masks_filenames = tf.constant(mask_list)
+
+dataset = tf.data.Dataset.from_tensor_slices((image_filenames, masks_filenames))
+
+for image, mask in dataset.take(1):
+    print(image)
+    print(mask)
+
+image_ds = dataset.map(process_path)
+processed_image_ds = image_ds.map(preprocess)
---
+++ lab22/utils.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+import numpy as np
+import struct
+import tensorflow as tf
+
+
+def get_this_file_dir():
+   return os.path.dirname(os.path.abspath(__file__))
+
+def process_path(image_path, mask_path):
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_png(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    mask = tf.io.read_file(mask_path)
+    mask = tf.image.decode_png(mask, channels=3)
+    mask = tf.math.reduce_max(mask, axis=-1, keepdims=True)
+    return img, mask
+
+def preprocess(image, mask):
+    input_image = tf.image.resize(image, (96, 128), method='nearest')
+    input_mask = tf.image.resize(mask, (96, 128), method='nearest')
+
+    input_image = input_image / 255.
+
+    return input_image, input_mask
+
